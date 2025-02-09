d8a6a4378029deee6f10e297ab9ae0ad2c425afd
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-09 12:04:56 +0500

    passed 3 tasks of 1st Chapter

---
+++ Lab3/Task1/JS_Fundamentals/variables.js
@@ -0,0 +1,19 @@
+//1
+let admin, name;
+name = 'John';
+admin = name;
+alert(admin);
+//2
+let ourPlanetName;
+ourPlanetName = "Earth";
+//3
+let curVisitor;
+curVisitor = "Aigerim;"
+//4
+const birthday = '18.04.1982';
+const age = someCode(birthday);
+
+const BIRTHDAY = '18.04.1982';
+const age = someCode(BIRTHDAY);
+
+
