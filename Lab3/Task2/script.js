3c651d96a54d97c2797fb0939fe7f88a15dbf9d2
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-09 20:39:27 +0500

    added checkbox type to the liststs when it will add to the input

--- Lab3/Task2/script.js
+++ Lab3/Task2/script.js
@@ -2,6 +2,11 @@ function addTask() {
     let input = document.getElementById("taskInput");
     let task = input.value.trim();
     if (!task) return;
+
+    let li = document.createElement("li");
+    let checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.onchange = () => li.classList.toggle("completed");
     
     
 }
