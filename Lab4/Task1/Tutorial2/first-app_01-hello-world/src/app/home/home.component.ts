add1c464ca78e5c1d777ff822f90bb560018e321
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-12 10:21:58 +0500

    created housing location component files

--- Lab4/Task1/Tutorial2/first-app_01-hello-world/src/app/home/home.component.ts
+++ Lab4/Task1/Tutorial2/first-app_01-hello-world/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,9 @@ import { Component } from '@angular/core';
         <input type = "text" placeholder = "Filter by city" />
         <button class = "primary" type = "button">Search</button>
       </form>
+    </section>
+    <section class = "results">
+      <app-housing-location></app-housing-location>
     </section>
   `,
   styleUrls: ['./home.component.css'],
