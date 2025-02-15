f30429816525a6cb26d4596e074531d963672bd4
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-16 01:55:48 +0500

    changed button to a , because button was not working

--- Lab4/Task2/Project/src/app/productitem/productitem.component.ts
+++ Lab4/Task2/Project/src/app/productitem/productitem.component.ts
@@ -4,18 +4,27 @@ import { ProductItem } from '../productitem';
 
 @Component({
   selector: 'app-product-item',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './productitem.component.html',
   styleUrl: './productitem.component.css'
 })
 export class ProductItemComponent {
   @Input() productItem!: ProductItem;
-}
 
-shareOnTelega(name: string, link: string){
-  link = encodeURIComponent(link);
-  text = encodeURIComponent(name);
-  const tgLink = "https://telegram.me/share/url?url=${link}&text=${text}";
-  window.open(tgLink, "_blank");
+  shareOnTelega(name: string, link: string){
+    const encodedlink = encodeURIComponent(link);
+    const text = encodeURIComponent(name);
+    const tgLink = `https://telegram.me/share/url?url=${encodedlink}&text=${text}`;
+    window.open(tgLink, "_blank");
+  }
+  
+  shareOnWhatsapp(name: string, link: string){
+    const encodedlink = encodeURIComponent(link);
+
+    const waLink = `https:/whatsapp://send?text=${encodedlink}`;
+    window.open(waLink, "_blank");
+  }
+
 }
 
