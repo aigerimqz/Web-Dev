6178f1dca7939f968e71ba5d56642241aee8d7fe
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-16 14:27:55 +0500

    added another some products photo

--- Lab4/Task2/Project/src/app/products/products.component.ts
+++ Lab4/Task2/Project/src/app/products/products.component.ts
@@ -41,7 +41,7 @@ export class ProductsComponent {
     name: 'Celimax Dual Barrier Creamy Toner',
     image: 'assets/products/4.jpg',
     description: 'Celimax Dual Barrier Creamy Toner hydrates, nourishes, strengthens the skin barrier, and soothes dryness and irritation.',
-    rating: 5.0,
+    rating: 4.5,
     link: 'https://kaspi.kz/shop/p/celimax-toner-dual-barrier-150-ml-108098827/?c=750000000'
   },
   {
@@ -49,7 +49,7 @@ export class ProductsComponent {
     name: 'Anua Dark Spot Correcting Serum',
     image: 'assets/products/5.jpg',
     description: 'Brightening serum with niacinamide and tranexamic acid targets hyperpigmentation and evens skin tone.',
-    rating: 5.0,
+    rating: 4.6,
     link: 'https://kaspi.kz/shop/p/anua-syvorotka-kontsentrat-dark-spot-correcting-10-niacinamide-4-tranexamic-acid-dlja-litsa-30-ml-118426606/?c=750000000'
   },
   {
@@ -57,7 +57,7 @@ export class ProductsComponent {
     name: 'Anua Peach 70% Niacin Serum',
     image: 'assets/products/6.jpg',
     description: 'Peach serum smooths, refines texture, tightens pores, controls oil, and brightens skin tone.',
-    rating: 5.0,
+    rating: 4.7,
     link: 'https://kaspi.kz/shop/p/anua-syvorotka-peach-70-niacin-dlja-litsa-30-ml-114423608/?c=750000000'
   },
   {
@@ -65,7 +65,7 @@ export class ProductsComponent {
     name: 'Anua Heartleaf Quercetinol Deep Pore Cleansing Foam',
     image: 'assets/products/7.jpg',
     description: 'Creamy scrub with Houttuynia leaf particles gently exfoliates, smooths skin, tightens pores, and has a refreshing eucalyptus scent.',
-    rating: 5.0,
+    rating: 4.8,
     link: 'https://kaspi.kz/shop/p/anua-heartleaf-quercetinol-deep-pore-cleansing-foam-penka-150-ml-114466905/?c=750000000'
   },
   {
@@ -73,7 +73,7 @@ export class ProductsComponent {
     name: 'Centella Probio-Cica Intensive Ampoule',
     image: 'assets/products/8.jpg',
     description: 'High-concentration probiotic ampoule restores, hydrates, nourishes, and provides anti-aging benefits.',
-    rating: 5.0,
+    rating: 4.7,
     link: 'https://kaspi.kz/shop/p/skin1004-ampul-naja-syvorotka-madagascar-centella-probio-cica-intensive-ampoule-dlja-litsa-95-ml-118889632/?c=750000000'
   },
   {
@@ -89,7 +89,7 @@ export class ProductsComponent {
     name: 'Beauty of Joseon Relief Sun Rice Probiotic SPF50+',
     image: 'assets/products/10.jpg',
     description: 'Probiotic sunscreen protects against UV damage, soothes, brightens, and supports the skin barrier. Lightweight, non-sticky, and absorbs for a natural glow without a white cast.',
-    rating: 5.0,
+    rating: 4.9,
     link: 'https://kaspi.kz/shop/p/beauty-of-joseon-krem-relief-sun-rice-probiotics-spf50-dlja-litsa-50-ml-105587876/?c=750000000'
   },
   {
@@ -97,7 +97,7 @@ export class ProductsComponent {
     name: 'Anua Hearleaf Pore Control Cleansing Oil',
     image: 'assets/products/11.jpg',
     description: 'Cleansing Oil gently removes makeup, unclogs pores, controls sebum, and refreshes skin with avocado oil.',
-    rating: 5.0,
+    rating: 4.7,
     link: 'https://kaspi.kz/shop/p/anua-heartleaf-pore-control-cleansing-oil-gidrofil-noe-maslo-200-ml-114393219/?c=750000000'
   }
   ];
