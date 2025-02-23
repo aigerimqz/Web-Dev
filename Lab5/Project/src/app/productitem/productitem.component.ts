f8429ca95e6014d87644222094a3a8ab9abf5f2a
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-02-23 05:11:36 +0500

    added new eventemitter for removing the items

--- Lab5/Project/src/app/productitem/productitem.component.ts
+++ Lab5/Project/src/app/productitem/productitem.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductItem } from '../productitem';
 
@@ -11,6 +11,7 @@ import { ProductItem } from '../productitem';
 })
 export class ProductItemComponent {
   @Input() productItem!: ProductItem;
+  @Output() remove = new EventEmitter<ProductItem>();
 
   getRatingStars(rating: number): number[]{
     return Array(Math.floor(rating)).fill(0);
