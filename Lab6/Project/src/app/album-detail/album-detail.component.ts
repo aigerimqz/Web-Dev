38b0e79025e0ba1a83800ef03b6444ef4f38f95c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-02 04:20:49 +0500

    album details work

--- Lab6/Project/src/app/album-detail/album-detail.component.ts
+++ Lab6/Project/src/app/album-detail/album-detail.component.ts
@@ -3,16 +3,19 @@ import { ActivatedRoute } from '@angular/router';
 import { Album } from '../album';
 import { Location } from '@angular/common';
 import { AlbumService } from '../album.service';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-album-detail',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './album-detail.component.html',
   styleUrl: './album-detail.component.css'
 })
 export class AlbumDetailComponent implements OnInit{
   album: Album | undefined
+  loaded: boolean = true;
 
-  constructor(private router: ActivatedRoute,
+  constructor(
+    private router: ActivatedRoute,
     private location: Location,
     private albumService: AlbumService){
 
@@ -27,7 +30,8 @@ export class AlbumDetailComponent implements OnInit{
 
   getAlbum(id: number){
     this.albumService.getAlbum(id).subscribe((album) => {
-      this.album = this.album;
+      this.album = album;
+      this.loaded = true;
     })
   }
 
