dd8a368ce6c5e981c47c36bff2807d2f60eebad1
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-16 19:03:01 +0500

    added tasks between a-e from theme uslovnye opeartory

---
+++ Lab7/Informatics/uslovnyi_operator.py
@@ -0,0 +1,46 @@
+#Task: a
+# x = int(input())
+# y = int(input())
+
+# if x > y:
+#     print(x)
+# else:
+#     print(y)
+
+#Task: b
+# year = int(input())
+# if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+#     print("YES")
+# else:
+#     print("NO")
+
+#Task: c
+# ans1 = int(input())
+# ans2 = int(input())
+
+# if(ans2 != 1 and ans1 != 1):
+#     print("YES")
+# elif(ans1 == 1 and ans2 == 1):
+#     print("YES")
+# else:
+#     print("NO")
+
+#Task: d
+# x = int(input())
+# if x > 0:
+#     print(1)
+# elif x < 0:
+#     print(-1)
+# else:
+#     print(0)
+
+
+#Task: e
+x = int(input())
+y = int(input())
+if x > y:
+    print(1)
+elif y > x:
+    print(2)
+else:
+    print(0)
