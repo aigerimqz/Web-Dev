8226b398d2024d70cca1b0c5352a6f1de344f43c
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-16 15:04:44 +0500

    started doing Lab #7

--- Lab6/Project/src/app/about/about.component.ts
+++ Lab6/Project/src/app/about/about.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  
   templateUrl: './about.component.html',
   styleUrl: './about.component.css'
 })
---
+++ Lab7/Informatics/a.py
@@ -0,0 +1,5 @@
+a = int(input())
+b = int(input())
+
+res = (a**2 + b**2)**(1/2)
+print(res)
