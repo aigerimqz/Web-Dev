a6f03bb29aa0a714061bddce6afdfd18829957b1
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-22 22:55:08 +0500

    completed all 10 task from HackerRank part

--- Lab7/HackerRank/tasks.py
+++ Lab7/HackerRank/tasks.py
@@ -28,28 +28,14 @@
 # print(a/b)
 
 
-#Meduim tasks
 #4
-# def minion_game(s):
-#     kev = 0
-#     stu = 0
-#     for i in range(len(s)):
-#         temp = ""
-#         if s[i] in vowels:
-#             for j in range(len(s)):
-#                 if temp in
-#         else:
+def print_full_name(first, last):
+    res = f"Hello {first} {last}! You just delved into python."
+    print(res)
 
-
-    
-#     return v
-
-    
-
-# s = input()
-# vowels = "AEIOU"
-
-# print(minion_game(s))
+first_name = input()
+last_name = input()
+print_full_name(first_name, last_name)
 
 #5
 # def is_leap(year):
@@ -70,6 +56,49 @@
 # print(f'{ans}{chr(176)}')
 
 #7
-n = int(input())
-for i in range(1, n + 1):
-    print(pow((10**i)//9, 2))
+# n = int(input())
+# arr = map(int, input().split())
+# s = list(set(arr))
+# s.sort()
+# print(s[-2])
+
+#8
+# n = int(input())
+# student_marks = {}
+# for _ in range(n):
+#     name, *line = input().split()
+#     scores = list(map(float, line))
+#     student_marks[name] = scores
+# query_name = input()
+# res = 0
+# for score in student_marks[query_name]:
+#     res += score
+
+# ans = res/len(student_marks[query_name])
+# print(f"{ans:.2f}")
+
+
+
+#9
+# n = int(input())
+# res = ""
+# for i in range(1, n + 1):
+#     res += str(i)
+# print(res)
+
+
+#10
+def swap_case(s):
+    result = "" 
+    for char in s:
+        if char.islower():
+            result += char.upper()  
+        elif char.isupper():
+            result += char.lower() 
+        else:
+            result += char 
+    return result
+
+s = input()
+result = swap_case(s)
+print(result)
