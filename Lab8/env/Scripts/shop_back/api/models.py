5a565f2e876631db36bb5afe9a91a71766f5a350
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-29 14:27:14 +0500

    added foreign key to product class to connect category and product

--- Lab8/env/Scripts/shop_back/api/models.py
+++ Lab8/env/Scripts/shop_back/api/models.py
@@ -1,22 +1,6 @@
 from django.db import models
 
 # Create your models here.
-class Product(models.Model):
-    name = models.CharField(max_length=100)
-    price = models.FloatField()
-    description = models.TextField(default='')
-    count = models.IntegerField()
-    is_active = models.BooleanField()
-
-    def to_json(self):
-        return {
-            "id": self.id,
-            "name": self.name,
-            "price": self.price,
-            "description": self.description,
-            "count": self.count,
-            "is_active": self.is_active
-        }
 
 
 class Category(models.Model):
@@ -27,3 +11,23 @@ class Category(models.Model):
             "id": self.id,
             "name": self.name
         }
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField(default='')
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "price": self.price,
+            "description": self.description,
+            "count": self.count,
+            "is_active": self.is_active,
+            "category_id": self.category.id
+        }
+
