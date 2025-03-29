5a6cfffe3f7ef78a3df0ceddef12cd4b8a7d6089
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-29 13:32:45 +0500

    added models

--- Lab8/env/Scripts/shop_back/api/models.py
+++ Lab8/env/Scripts/shop_back/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField(default='')
+    count = models.IntegerField()
+    is_active = models.BooleanField()
+    
