e07befcb690c2a9af98f78f61319fff2a3c38756
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-29 15:58:48 +0500

    added models

--- Lab9/myenv/Scripts/hh_back/api/models.py
+++ Lab9/myenv/Scripts/hh_back/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Models):
+    name = models.CharField(max_length=100)
+    description = models.TextField(default='')
+    city = models.CharField(max_length=100),
+    address = models.TextField()
+
+class Vacancy(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    salary = models.FloatField()
+    company = models.ForeignKey(Company)
