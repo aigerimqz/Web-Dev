407637109a097856e45a4c5f7888492844c17e5f
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-03-29 13:47:37 +0500

    tested json

--- Lab8/env/Scripts/shop_back/api/urls.py
+++ Lab8/env/Scripts/shop_back/api/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
+    path('products/', views.test_json)
    
 ]
--- Lab8/env/Scripts/shop_back/api/views.py
+++ Lab8/env/Scripts/shop_back/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from api.models import Product
+from django.http import HttpRequest, HttpResponse, JsonResponse
 
 # Create your views here.
+def test_json(request):
+    data = {
+        "name": "Aigerim",
+        "lastname": "Manat",
+        "gpa": 3.9
+    }
+    return JsonResponse(data)
