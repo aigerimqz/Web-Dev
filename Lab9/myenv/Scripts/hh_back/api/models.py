418365ccc3e3087d25f5def8d2e6438dc1900414
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-04 16:28:52 +0500

    made new migrations with changes in models

--- Lab9/myenv/Scripts/hh_back/api/models.py
+++ Lab9/myenv/Scripts/hh_back/api/models.py
@@ -4,12 +4,26 @@ from django.db import models
 class Company(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(default='')
-    city = models.CharField(max_length=100),
-    address = models.TextField()
-    new = models.CharField()
+    city = models.CharField(max_length=100)
+    address = models.TextField(default='')
+    new = models.CharField(max_length=100)
 
 class Vacancy(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField()
+    description = models.TextField(default='')
     salary = models.FloatField()
     company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name="company")
+
+
+# def __str__(self):
+#     return self.name
+
+# def to_json(self):
+#     return{
+#         "id": self.id,
+#         "name": self.name,
+#         "description": self.description,
+#         "city": self.city,
+#         "address": self.address,
+
+#     }
