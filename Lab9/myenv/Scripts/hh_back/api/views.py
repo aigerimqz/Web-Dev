e2f01c92253166b384e715cdb3a274ef8a005056
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-06 15:50:50 +0500

    added company_detail function with post put delete get requests with serializers

--- Lab9/myenv/Scripts/hh_back/api/views.py
+++ Lab9/myenv/Scripts/hh_back/api/views.py
@@ -21,10 +21,30 @@ def companies_list(request):
             return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
 
-def companies_show(request, company_id = None):
+@csrf_exempt
+def company_detail(request, company_id = None):
     try:
         company = Company.objects.get(pk = company_id)
     except Company.DoesNotExist as e:
-        return JsonResponse({'error': str(e)}, status = 404)
+        return JsonResponse({'error': str(e)}, status=404)
+    if request.method == "GET":
+        serializer = CompanySerializer(company)
+        return JsonResponse(serializer.data, status=200)
+    elif request.method == "PUT":
+        new_data = json.loads(request.body)
+        serializer = CompanySerializer(instance = company, data = new_data)
 
-    return JsonResponse(company.to_json(), status = 200)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
+    elif request.method == "DELETE":
+        company.delete()
+        return JsonResponse({'message': 'Company deleted'})
+# def companies_show(request, company_id = None):
+#     try:
+#         company = Company.objects.get(pk = company_id)
+#     except Company.DoesNotExist as e:
+#         return JsonResponse({'error': str(e)}, status = 404)
+
+#     return JsonResponse(company.to_json(), status = 200)
