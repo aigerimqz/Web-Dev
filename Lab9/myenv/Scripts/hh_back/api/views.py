a11c28801cc22b55cb61ad57e468ad956701b6e2
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-06 17:35:18 +0500

    added view functions for the vacancy model

--- Lab9/myenv/Scripts/hh_back/api/views.py
+++ Lab9/myenv/Scripts/hh_back/api/views.py
@@ -41,10 +41,40 @@ def company_detail(request, company_id = None):
     elif request.method == "DELETE":
         company.delete()
         return JsonResponse({'message': 'Company deleted'})
-# def companies_show(request, company_id = None):
-#     try:
-#         company = Company.objects.get(pk = company_id)
-#     except Company.DoesNotExist as e:
-#         return JsonResponse({'error': str(e)}, status = 404)
 
-#     return JsonResponse(company.to_json(), status = 200)
+
+
+@csrf_exempt
+def vacancies_list(request):
+    if request.method == "GET":
+        vacancies = Vacancy.objects.all()
+        serializer = VacancySerializer(vacancies, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    elif request.method == "POST":
+        data = json.loads(request.body)
+        serializer = VacancySerializer(data = data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+@csrf_exempt
+def vacancy_detail(request, company_id = None):
+    try:
+        vacancy = Vacancy.objects.get(pk = company_id)
+    except Vacancy.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=404)
+    if request.method == "GET":
+        serializer = VacancySerializer(vacancy)
+        return JsonResponse(serializer.data, status=200)
+    elif request.method == "PUT":
+        new_data = json.loads(request.body)
+        serializer = VacancySerializer(instance = vacancy, data = new_data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status = 201)
+        return JsonResponse(serializer.errors, status = 400)
+    elif request.method == "DELETE":
+        vacancy.delete()
+        return JsonResponse({'message': 'Vacancy deleted'})
