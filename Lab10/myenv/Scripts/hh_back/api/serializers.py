5f8442c1a022eff80f3ea008c0d05de16207a31d
aigerim <110599316+aigerimqz@users.noreply.github.com>
2025-04-13 04:02:40 +0500

    user serializers.Serilizer method and also serilizers.ModelSerializers

--- Lab10/myenv/Scripts/hh_back/api/serializers.py
+++ Lab10/myenv/Scripts/hh_back/api/serializers.py
@@ -1,10 +1,11 @@
 from rest_framework import serializers
 from api.models import Company, Vacancy
 
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = '__all__'
+class CompanySerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    name = serializers.CharField()
+    description = serializers.CharField()
+    city = serializers.CharField()
 
 class VacancySerializer(serializers.ModelSerializer):
     class Meta:
